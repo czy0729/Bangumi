cd5c1c29ac920f81899d69f7abe695035cfa4ab5
czy0729 <402731062@qq.com>
2020-02-16 08:29:45 +0800

    - [x] [条目] 评分增加标准差和争议度

--- screens/home/subject/rating.js
+++ screens/home/subject/rating.js
@@ -2,7 +2,7 @@
  * @Author: czy0729
  * @Date: 2019-03-24 05:29:31
  * @Last Modified by: czy0729
- * @Last Modified time: 2020-01-19 16:03:42
+ * @Last Modified time: 2020-02-16 08:26:45
  */
 import React from 'react'
 import { View } from 'react-native'
@@ -11,19 +11,8 @@ import { observer } from 'mobx-react'
 import { Flex, Text, Touchable, Iconfont } from '@components'
 import { SectionTitle } from '@screens/_'
 import { _ } from '@stores'
-import { open } from '@utils'
+import { open, toFixed } from '@utils'
 import { t } from '@utils/fetch'
-
-function getHeight(total, current) {
-  if (!total) {
-    return 0
-  }
-  let percent = current / total
-  if (percent > 0 && percent < 0.01) {
-    percent = 0.01
-  }
-  return `${percent * 100}%`
-}
 
 class Ranting extends React.Component {
   static contextTypes = {
@@ -38,6 +27,20 @@ class Ranting extends React.Component {
     this.setState({
       show: true
     })
+
+  /**
+   * 标准差
+   */
+  get deviation() {
+    const { $ } = this.context
+    const { total, count, score } = $.rating
+    if (total == 0) {
+      return 0
+    }
+
+    const scores = Object.values(count).reverse()
+    return calculateSD(scores, score, total)
+  }
 
   renderTitle() {
     const { $ } = this.context
@@ -100,10 +103,15 @@ class Ranting extends React.Component {
           <Text size={12} type='main'>
             {$.rating.score}
           </Text>{' '}
-          / {$.rating.total} votes / Ranked:{' '}
+          / {$.rating.total} votes / Ranked{' '}
           <Text size={12} type='main'>
             #{rank}
-          </Text>
+          </Text>{' '}
+          / 标准差{' '}
+          <Text size={12} type='main'>
+            {toFixed(this.deviation, 2)}
+          </Text>{' '}
+          / {getDispute(this.deviation)}
         </Text>
         {!!friend.score && (
           <Text style={_.mt.sm} size={12}>
@@ -164,3 +172,65 @@ const memoStyles = _.memoStyles(_ => ({
     height: 120
   }
 }))
+
+/**
+ * 比例柱子高度
+ * @param {*} total
+ * @param {*} current
+ */
+function getHeight(total, current) {
+  if (!total) {
+    return 0
+  }
+  let percent = current / total
+  if (percent > 0 && percent < 0.01) {
+    percent = 0.01
+  }
+  return `${percent * 100}%`
+}
+
+/**
+ * 计算标准差
+ * @param {*} scores
+ * @param {*} score
+ * @param {*} n
+ */
+function calculateSD(scores, score, n) {
+  let sd = 0
+  scores.forEach((item, index) => {
+    if (item === 0) {
+      return
+    }
+    sd += (10 - index - score) * (10 - index - score) * item
+  })
+  return Math.sqrt(sd / n)
+}
+
+/**
+ * 计算争议度
+ * @param {*} deviation
+ */
+function getDispute(deviation) {
+  if (deviation === 0) {
+    return '-'
+  }
+  if (deviation < 1) {
+    return '异口同声'
+  }
+  if (deviation < 1.15) {
+    return '基本一致'
+  }
+  if (deviation < 1.3) {
+    return '略有分歧'
+  }
+  if (deviation < 1.45) {
+    return '莫衷一是'
+  }
+  if (deviation < 1.6) {
+    return '各执一词'
+  }
+  if (deviation < 1.75) {
+    return '你死我活'
+  }
+  return '厨黑大战'
+}
