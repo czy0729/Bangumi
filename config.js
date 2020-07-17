5c0d85823a9b1a0aeece69d8c43f887975cc22c1
czy0729 <402731062@qq.com>
2020-07-17 12:53:20 +0800

    no message

---
+++ config.js
@@ -0,0 +1,32 @@
+/*
+ * @Author: czy0729
+ * @Date: 2019-06-02 14:42:28
+ * @Last Modified by: czy0729
+ * @Last Modified time: 2020-07-17 11:19:36
+ */
+export const initialDevDark = '' // '' 不控制 | true 强制黑暗 | false 强制白天
+export const initialHomeTabName = 'Home'
+
+export default {
+  initialRouteName: 'HomeTab', // HomeTab Discovery TinygrailCharaAssets
+  initialRouteParams: {
+    // id: 240929
+    // blogId: 295843 // 294448
+    // catalogId: 29811,
+    // groupId: 'tinygrail',
+    // monoId: 'person/7498' // 20391 character/70323 person/5745
+    // subjectId: 148726 // 103824 238923 279457 100444 204135
+    // jp: 'ようこそ実力至上主義の教室へ',
+    // cn: '欢迎来到实力至上主义的教室',
+    // topicId: 'subject/17618' // group/357093, ep/768491
+    // userId: '486811' // 456208, 419012
+    // userName: 'ekibun'
+    // from: 'tinygrail',
+    // form: 'lottery', // lottery
+    // message: '彩票刮刮乐共获得： #20391「双叶杏」64股 #70900「神原骏河」36股',
+    // name: 'とある科学の超電磁砲T'
+    // tag: '水树奈奈',
+    // type: 'asks' // 'anime'
+    // uri: 'https://bgm.tv/award/2019',
+  }
+}
