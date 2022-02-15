ced6134bf73f37d2d10c9e1b56678146e0a417ba
czy0729 <402731062@qq.com>
2022-02-15 22:10:48 +0800

    - [x] [帖子] 现在楼层内的小图片会直接加载显示

--- src/components/render-html/toggle-image.js
+++ src/components/render-html/toggle-image.js
@@ -2,19 +2,19 @@
  * @Author: czy0729
  * @Date: 2019-08-14 10:15:24
  * @Last Modified by: czy0729
- * @Last Modified time: 2022-02-12 09:21:09
+ * @Last Modified time: 2022-02-15 22:07:08
  */
 import React from 'react'
-import { StyleSheet, View } from 'react-native'
+import { View } from 'react-native'
 import { observer } from 'mobx-react'
 import ActivityIndicator from '@ant-design/react-native/lib/activity-indicator'
+import { Iconfont } from '@components'
 import { _ } from '@stores'
 import { open } from '@utils'
 import axios from '@utils/thirdParty/axios'
 import { Flex } from '../flex'
 import { Image } from '../image'
 import { Touchable } from '../touchable'
-import { Iconfont } from '../iconfont'
 import { Text } from '../text'
 
 const memoSize = {}
@@ -78,112 +78,120 @@ class ToggleImage extends React.Component {
       loaded: true
     })
 
+  get isIcon() {
+    const { src } = this.props
+    if (typeof src !== 'string') return false
+    if (src.includes('https://static.saraba1st.com/image/smiley/')) return true
+
+    const { size } = this.state
+    if (size && size <= 50) return true
+  }
+
   render() {
     // RN不使用第三方link包暂时不支持webp, 暂时使用浏览器跳转
     const { src, onImageFallback } = this.props
-    const isRemote = typeof src === 'string'
-    if (isRemote && src.includes('.webp')) {
-      return (
-        <Touchable style={this.styles.image} onPress={onImageFallback}>
-          <Flex
-            style={this.styles.imagePlaceholder}
-            direction='column'
-            justify='center'
-          >
-            <Text size={10} type='sub'>
-              框架暂不支持webp图片, 使用浏览器打开
-            </Text>
-            {isRemote && (
-              <Text
-                style={this.styles.textSrc}
-                size={10}
-                type='sub'
-                selectable
-                numberOfLines={1}
-              >
-                {src}
-              </Text>
-            )}
-          </Flex>
-        </Touchable>
-      )
-    }
-
     const { show, loaded, size } = this.state
-    const ext = src.includes('.jpg')
-      ? 'jpg'
-      : src.includes('.png')
-      ? 'png'
-      : src.includes('.gif')
-      ? 'gif'
-      : ''
-    if (!show) {
-      const text = []
-      if (ext) text.push(`[${ext}]`)
-      if (size === 0) {
-        text.push('[获取信息失败]')
-      } else {
-        text.push(`[${size}kb]`)
-      }
-      return (
-        <Touchable
-          style={this.styles.image}
-          onPress={this.toggleShow}
-          onLongPress={() => open(src)}
-        >
-          <Flex
-            style={this.styles.imagePlaceholder}
-            direction='column'
-            justify='center'
-          >
-            <Text size={12} type='sub' bold>
-              {text.join(' ')}
-            </Text>
-            {isRemote && (
-              <Text
-                style={this.styles.textSrc}
-                size={10}
-                type='sub'
-                selectable
-                numberOfLines={1}
-              >
-                {src}
+
+    if (!this.isIcon) {
+      const isRemote = typeof src === 'string'
+      if (isRemote && src.includes('.webp')) {
+        return (
+          <Touchable style={this.styles.image} onPress={onImageFallback}>
+            <Flex
+              style={this.styles.imagePlaceholder}
+              direction='column'
+              justify='center'
+            >
+              <Text size={10} type='sub'>
+                框架暂不支持webp, 使用浏览器打开
               </Text>
-            )}
-          </Flex>
-        </Touchable>
-      )
+              {isRemote && (
+                <Text
+                  style={this.styles.textSrc}
+                  size={10}
+                  type='sub'
+                  selectable
+                  numberOfLines={1}
+                >
+                  {src}
+                </Text>
+              )}
+            </Flex>
+          </Touchable>
+        )
+      }
+
+      const ext = src.includes('.jpg')
+        ? 'jpg'
+        : src.includes('.png')
+        ? 'png'
+        : src.includes('.gif')
+        ? 'gif'
+        : ''
+      if (!show) {
+        const text = []
+        if (ext) text.push(`[${ext}]`)
+        if (size === 0) {
+          text.push('[获取大小失败]')
+        } else {
+          text.push(`[${size}kb]`)
+        }
+        return (
+          <Touchable
+            style={[this.styles.image, this.styles.isLoad]}
+            onPress={this.toggleShow}
+            onLongPress={() => open(src)}
+          >
+            <Flex
+              style={this.styles.imagePlaceholder}
+              direction='column'
+              justify='center'
+            >
+              <Text size={11} type='sub' bold>
+                {text.join(' ')}
+              </Text>
+              {isRemote && (
+                <Text
+                  style={this.styles.textSrc}
+                  size={10}
+                  type='sub'
+                  selectable
+                  numberOfLines={1}
+                >
+                  {src}
+                </Text>
+              )}
+            </Flex>
+          </Touchable>
+        )
+      }
     }
 
     return (
-      <Flex style={this.styles.image} justify='center'>
+      <Flex
+        style={[this.styles.image, !loaded && this.styles.isLoad]}
+        justify={this.isIcon ? 'start' : 'center'}
+      >
         <Image
           {...this.props}
+          autoSize={this.props.autoSize ? this.props.autoSize - _.sm : undefined}
           radius
           onLoadEnd={this.onLoadEnd}
           onError={this.onLoadEnd}
         />
-        {!this.props.show && (
-          <View style={this.styles.closeImageWrap}>
-            <Touchable onPress={this.toggleShow} onLongPress={() => open(src)}>
-              <Flex style={this.styles.closeImage} justify='center'>
-                <Iconfont size={18} name='md-close' color={_.__colorPlain__} />
-              </Flex>
-            </Touchable>
-          </View>
-        )}
         {!loaded && (
-          <Flex
-            style={[
-              StyleSheet.absoluteFill,
-              {
-                width: this.props.autoSize
-              }
-            ]}
-            justify='center'
-          >
-            <ActivityIndicator size='small' color={_.colorIcon} />
-          </Flex>
+          <>
+            <View style={this.styles.closeImageWrap}>
+              <Touchable onPress={this.toggleShow} onLongPress={() => open(src)}>
+                <Flex style={this.styles.closeImage} justify='center'>
+                  <Iconfont size={16} name='md-close' color={_.colorSub} />
+                </Flex>
+              </Touchable>
+            </View>
+            <Flex style={this.styles.loading} justify='center'>
+              <ActivityIndicator size='small' color={_.colorIcon} />
+            </Flex>
+          </>
         )}
       </Flex>
     )
@@ -196,24 +204,37 @@ class ToggleImage extends React.Component {
 
 const memoStyles = _.memoStyles(() => ({
   image: {
-    marginVertical: _.sm
+    marginVertical: 6
+  },
+  isLoad: {
+    width: _.window.contentWidth * 0.64,
+    height: 64 * _.ratio,
+    borderRadius: _.radiusSm,
+    overflow: 'hidden'
   },
   loading: {
-    width: 32,
-    height: 32
+    position: 'absolute',
+    zIndex: 1,
+    top: 0,
+    left: 0,
+    width: _.window.contentWidth * 0.64,
+    height: 64 * _.ratio
   },
   imagePlaceholder: {
-    width: '100%',
-    height: 96 * _.ratio,
+    width: _.window.contentWidth * 0.64,
+    height: 64 * _.ratio,
     borderWidth: 1,
     borderColor: _.colorBorder,
-    borderRadius: _.radiusXs
+    borderRadius: _.radiusSm,
+    overflow: 'hidden'
   },
   closeImageWrap: {
     position: 'absolute',
-    zIndex: 1,
-    top: 12,
-    right: 8
+    zIndex: 2,
+    top: 4,
+    right: 4,
+    borderRadius: 12,
+    overflow: 'hidden'
   },
   closeImage: {
     width: 24,
@@ -223,7 +244,7 @@ const memoStyles = _.memoStyles(() => ({
     overflow: 'hidden'
   },
   textSrc: {
-    maxWidth: '64%',
-    marginTop: _.sm
+    maxWidth: '96%',
+    marginTop: _.xs
   }
 }))
--- src/screens/discovery/index/award-2021.js
+++ src/screens/discovery/index/award-2021.js
@@ -2,7 +2,7 @@
  * @Author: czy0729
  * @Date: 2022-02-14 06:57:31
  * @Last Modified by: czy0729
- * @Last Modified time: 2022-02-15 20:20:56
+ * @Last Modified time: 2022-02-15 20:26:31
  */
 import React from 'react'
 import { View } from 'react-native'
@@ -11,6 +11,8 @@ import { Image, Touchable, Text } from '@components'
 import { _ } from '@stores'
 import { obc } from '@utils/decorators'
 import Img2021 from '@assets/images/year_2021.png'
+
+const jquery = 'https://cdn.bootcdn.net/ajax/libs/jquery/3.6.0/jquery.slim.min.js'
 
 function Award2021(props, { $ }) {
   const styles = memoStyles()
@@ -22,7 +24,7 @@ function Award2021(props, { $ }) {
           style={styles.body}
           source={{
             // eslint-disable-next-line max-len
-            html: `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width,initial-scale=1"><title>Bangumi Award 2021</title><style>*{padding:0;margin:0}::-webkit-scrollbar{display:none}body{overflow:hidden;background:#000}a{font-size:13px;text-decoration:none}.blockrain-score-holder{display:none!important}</style></head><body><div style="position:relative;width:${styles.body.width}px;height:${styles.body.height}px"><div class="SidePanelMini clearit" style="overflow:hidden;position:relative;box-sizing:border-box;border:none;padding:0;margin:0 0 10px 0;border:none;background-color:#c4cfa1;box-shadow:0 10px 20px rgba(0,0,0,.2);width:${styles.body.width}px;height:${styles.body.height}px;line-height:100%"><div style="position:absolute;width:100%;height:100%;z-index:1;color:#fff;background-color:#c4cfa1"><div><div class="screen_game" style="position:absolute;top:0;width:100%;height:100%;overflow:hidden"></div><div style="font-family:monospace,Pixelmix,'HanHei SC','PingFang SC','Helvetica Neue',Helvetica,STHeitiSC-Light,Arial,sans-serif;position:absolute;z-index:1;width:100%;height:100%;text-align:center;top:35%;font-size:18px;color:#3e4730;text-shadow:1px 1px #c4cfa1">2021 YEAR IN REVIEW</div></div></div></div></div><script src="https://cdn.jsdelivr.net/npm/jquery@3.3.1/dist/jquery.min.js"></script><script src="https://bgm.tv/js/jquery_blockrain.js?r351"></script><script>window.addEventListener("DOMContentLoaded",function(){$(".screen_game").blockrain({theme:"gameboy",blockWidth:18,showFieldOnStart:!1,autoplay:!0,autoplayRestart:!0,controls:!1})})</script></body></html>`
+            html: `<!DOCTYPE html><html lang="en"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width,initial-scale=1"><title>Bangumi Award 2021</title><style>*{padding:0;margin:0}::-webkit-scrollbar{display:none}body{overflow:hidden;background:#000}a{font-size:13px;text-decoration:none}.blockrain-score-holder{display:none!important}</style></head><body><div style="position:relative;width:${styles.body.width}px;height:${styles.body.height}px"><div class="SidePanelMini clearit" style="overflow:hidden;position:relative;box-sizing:border-box;border:none;padding:0;margin:0 0 10px 0;border:none;background-color:#c4cfa1;box-shadow:0 10px 20px rgba(0,0,0,.2);width:${styles.body.width}px;height:${styles.body.height}px;line-height:100%"><div style="position:absolute;width:100%;height:100%;z-index:1;color:#fff;background-color:#c4cfa1"><div><div class="screen_game" style="position:absolute;top:0;width:100%;height:100%;overflow:hidden"></div><div style="font-family:monospace,Pixelmix,'HanHei SC','PingFang SC','Helvetica Neue',Helvetica,STHeitiSC-Light,Arial,sans-serif;position:absolute;z-index:1;width:100%;height:100%;text-align:center;top:35%;font-size:18px;color:#3e4730;text-shadow:1px 1px #c4cfa1">2021 YEAR IN REVIEW</div></div></div></div></div><script src="${jquery}"></script><script src="https://bgm.tv/js/jquery_blockrain.js?r351"></script><script>window.addEventListener("DOMContentLoaded",function(){$(".screen_game").blockrain({theme:"gameboy",blockWidth:18,showFieldOnStart:!1,autoplay:!0,autoplayRestart:!0,controls:!1})})</script></body></html>`
           }}
           scrollEnabled={false}
           androidHardwareAccelerationDisabled
