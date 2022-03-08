ee67dd7fffff735b5c474e4f036045b4590ee9db
czy0729 <402731062@qq.com>
2022-03-08 16:05:07 +0800

    package

--- android/app/src/main/java/com/czy0729/bangumi/generated/BasePackageList.java
+++ android/app/src/main/java/com/czy0729/bangumi/generated/BasePackageList.java
@@ -8,19 +8,19 @@ public class BasePackageList {
   public List<Package> getPackageList() {
     return Arrays.<Package>asList(
         new expo.modules.constants.ConstantsPackage(),
-        new expo.modules.errorrecovery.ErrorRecoveryPackage(),
         new expo.modules.filesystem.FileSystemPackage(),
         new expo.modules.font.FontLoaderPackage(),
         new expo.modules.haptics.HapticsPackage(),
-        new expo.modules.imageloader.ImageLoaderPackage(),
         new expo.modules.keepawake.KeepAwakePackage(),
         new expo.modules.lineargradient.LinearGradientPackage(),
         new expo.modules.location.LocationPackage(),
-        new expo.modules.permissions.PermissionsPackage(),
         new expo.modules.screenorientation.ScreenOrientationPackage(),
         new expo.modules.splashscreen.SplashScreenPackage(),
+        new expo.modules.webbrowser.WebBrowserPackage(),
+        new expo.modules.errorrecovery.ErrorRecoveryPackage(),
+        new expo.modules.permissions.PermissionsPackage(),
         new expo.modules.sqlite.SQLitePackage(),
-        new expo.modules.webbrowser.WebBrowserPackage()
+        new expo.modules.imageloader.ImageLoaderPackage()
     );
   }
 }
