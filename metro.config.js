ac5584540e087d21c08c34f7462611c9bb7be23f
czy0729 <402731062@qq.com>
2023-01-07 23:55:47 +0800

    - publish v7.5.3

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,17 @@
-// Learn more https://docs.expo.io/guides/customizing-metro
-const { getDefaultConfig } = require('expo/metro-config')
+/**
+ * Metro configuration for React Native
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
 
-module.exports = getDefaultConfig(__dirname)
+module.exports = {
+  transformer: {
+    getTransformOptions: async () => ({
+      transform: {
+        experimentalImportSupport: false,
+        inlineRequires: false
+      }
+    })
+  }
+}
