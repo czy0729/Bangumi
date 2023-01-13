d1d0dbd2e1ce22cc77496ec2bc3c8c42c35d9c31
czy0729 <402731062@qq.com>
2023-01-13 08:06:08 +0800

    - [x] [条目] 评分块右上方新增网页新功能 (透视) 的入口

--- src/screens/home/subject/rating/title.tsx
+++ src/screens/home/subject/rating/title.tsx
@@ -2,18 +2,18 @@
  * @Author: czy0729
  * @Date: 2021-08-12 15:30:23
  * @Last Modified by: czy0729
- * @Last Modified time: 2022-08-26 11:34:29
+ * @Last Modified time: 2023-01-13 08:00:42
  */
 import React from 'react'
 import { Flex, Text, Touchable, Iconfont, Heatmap } from '@components'
 import { _, systemStore } from '@stores'
 import { SectionTitle, Rank } from '@_'
-import { open } from '@utils'
+import { cnjp } from '@utils'
 import { obc } from '@utils/decorators'
 import { t } from '@utils/fetch'
 import { Ctx } from '../types'
 
-function Title({ showScore }, { $ }: Ctx) {
+function Title({ showScore }, { $, navigation }: Ctx) {
   global.rerender('Subject.Rating.Title')
 
   const { showRating } = systemStore.setting
@@ -24,29 +24,56 @@ function Title({ showScore }, { $ }: Ctx) {
       left={showRating && <Rank style={styles.rank} value={rank} size={13} />}
       right={
         showRating && (
-          <Touchable
-            style={styles.rate}
-            onPress={() => {
-              t('条目.跳转', {
-                to: 'Netabare',
-                from: '评分分布',
-                subjectId: $.subjectId
-              })
-              open(`https://netaba.re/subject/${$.subjectId}`)
-            }}
-          >
-            <Flex>
-              {showNetabare && (
-                <>
+          <Flex>
+            {showNetabare && (
+              <Touchable
+                style={styles.netabare}
+                onPress={() => {
+                  t('条目.跳转', {
+                    to: 'Netabare',
+                    from: '评分分布',
+                    subjectId: $.subjectId
+                  })
+
+                  navigation.push('WebBrowser', {
+                    title: `${cnjp($.cn, $.jp)}的趋势`,
+                    url: `https://netaba.re/subject/${$.subjectId}`
+                  })
+                }}
+              >
+                <Flex>
                   <Text style={_.ml.sm} type='sub'>
                     趋势
                   </Text>
-                  <Iconfont style={_.ml.xs} name='md-open-in-new' size={18} />
-                </>
-              )}
-            </Flex>
-            <Heatmap id='条目.跳转' from='评分分布' />
-          </Touchable>
+                  <Iconfont style={_.ml.xs} name='md-open-in-new' size={17} />
+                </Flex>
+                <Heatmap id='条目.跳转' from='评分分布' to='Netabare' />
+              </Touchable>
+            )}
+            <Touchable
+              style={styles.rate}
+              onPress={() => {
+                t('条目.跳转', {
+                  to: 'Stats',
+                  from: '评分分布',
+                  subjectId: $.subjectId
+                })
+
+                navigation.push('WebBrowser', {
+                  title: `${cnjp($.cn, $.jp)}的透视`,
+                  url: `https://bgm.tv/subject/${$.subjectId}/stats`
+                })
+              }}
+            >
+              <Flex>
+                <Text style={_.ml.sm} type='sub'>
+                  透视
+                </Text>
+                <Iconfont style={_.ml.xs} name='md-open-in-new' size={17} />
+              </Flex>
+              <Heatmap id='条目.跳转' from='评分分布' to='Stats' />
+            </Touchable>
+          </Flex>
         )
       }
       icon={!showRating && 'md-navigate-next'}
@@ -65,8 +92,12 @@ function Title({ showScore }, { $ }: Ctx) {
 export default obc(Title)
 
 const styles = _.create({
+  netabare: {
+    borderRadius: _.radiusSm,
+    overflow: 'hidden'
+  },
   rate: {
-    paddingLeft: _.xs,
+    paddingLeft: _.sm,
     marginRight: -_.xs,
     borderRadius: _.radiusSm,
     overflow: 'hidden'
--- src/screens/web-view/web-browser/index.tsx
+++ src/screens/web-view/web-browser/index.tsx
@@ -2,20 +2,34 @@
  * @Author: czy0729
  * @Date: 2022-12-30 20:54:54
  * @Last Modified by: czy0729
- * @Last Modified time: 2022-12-30 21:28:19
+ * @Last Modified time: 2023-01-13 08:02:57
  */
-import React from 'react'
+import React, { useState } from 'react'
 import { Header } from '@components'
 import WebView from '@components/@/web-view'
+import { IconTouchable } from '@_'
+import { _ } from '@stores'
 
 const WebBrowser = ({ route }) => {
+  const [key, setKey] = useState(0)
+
   const { url, title } = route.params
   if (!url) return null
 
   return (
     <>
-      <Header title={title || '浏览器'} />
+      <Header
+        title={title || '浏览器'}
+        headerRight={() => (
+          <IconTouchable
+            name='md-refresh'
+            color={_.colorTitle}
+            onPress={() => setKey(key + 1)}
+          />
+        )}
+      />
       <WebView
+        key={String(key)}
         originWhitelist={['*']}
         source={{
           uri: url
