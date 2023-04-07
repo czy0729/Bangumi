635f864fb325549bc2ad506824b8ccb488fe5703
czy0729 <402731062@qq.com>
2023-04-08 03:33:52 +0800

    - [每日放送] 修复放送时间全都早了1个小时的问题

--- src/stores/calendar/onair.ts
+++ src/stores/calendar/onair.ts
@@ -7,198 +7,198 @@
 export const ON_AIR = {
   366331: {
     weekDayCN: 1,
-    timeCN: '1900'
+    timeCN: '2000'
   },
   323651: {
     weekDayCN: 1,
-    timeCN: '2000'
+    timeCN: '2100'
   },
   407713: {
-    weekDayCN: 1,
-    timeCN: '2330'
+    weekDayCN: 2,
+    timeCN: '0030'
   },
   378101: {
     weekDayCN: 2,
-    timeCN: '0000'
+    timeCN: '0100'
   },
   390712: {
     weekDayCN: 1,
-    timeCN: '1930'
+    timeCN: '2030'
   },
   365437: {
     weekDayCN: 1,
-    timeCN: '2200'
+    timeCN: '2300'
   },
   408013: {
     weekDayCN: 1,
-    timeCN: '2200'
+    timeCN: '2300'
   },
   357961: {
     weekDayCN: 2,
-    timeCN: '2100'
+    timeCN: '2200'
   },
   402136: {
     weekDayCN: 2,
-    timeCN: '2200'
+    timeCN: '2300'
   },
   407659: {
-    weekDayCN: 2,
-    timeCN: '2329'
+    weekDayCN: 3,
+    timeCN: '0029'
   },
   376703: {
     weekDayCN: 3,
-    timeCN: '2200'
+    timeCN: '2300'
   },
   385209: {
     weekDayCN: 3,
-    timeCN: '2200'
+    timeCN: '2300'
   },
   377607: {
     weekDayCN: 3,
-    timeCN: '2230'
+    timeCN: '2330'
   },
   386809: {
     weekDayCN: 3,
-    timeCN: '2100'
+    timeCN: '2200'
   },
   325673: {
     weekDayCN: 3,
-    timeCN: '2200'
+    timeCN: '2300'
   },
   399820: {
     weekDayCN: 4,
-    timeCN: '2000'
+    timeCN: '2100'
   },
   363101: {
     weekDayCN: 4,
-    timeCN: '2030'
+    timeCN: '2130'
   },
   381666: {
     weekDayCN: 4,
-    timeCN: '2030'
+    timeCN: '2130'
   },
   375816: {
     weekDayCN: 4,
-    timeCN: '2200'
+    timeCN: '2300'
   },
   397025: {
     weekDayCN: 4,
-    timeCN: '2230'
+    timeCN: '2330'
   },
   379639: {
-    weekDayCN: 4,
-    timeCN: '2300'
+    weekDayCN: 5,
+    timeCN: '0000'
   },
   394997: {
     weekDayCN: 4,
-    timeCN: '2255'
+    timeCN: '2355'
   },
   402935: {
     weekDayCN: 5,
-    timeCN: '2030'
+    timeCN: '2130'
   },
   389571: {
     weekDayCN: 5,
-    timeCN: '2200'
+    timeCN: '2300'
   },
   355798: {
-    weekDayCN: 5,
-    timeCN: '2305'
+    weekDayCN: 6,
+    timeCN: '0005'
   },
   389998: {
-    weekDayCN: 5,
-    timeCN: '2323'
+    weekDayCN: 6,
+    timeCN: '0023'
   },
   398943: {
-    weekDayCN: 5,
-    timeCN: '2325'
-  },
-  415587: {
-    weekDayCN: 5,
-    timeCN: '2335'
-  },
-  349039: {
-    weekDayCN: 5,
-    timeCN: '2355'
-  },
-  387803: {
     weekDayCN: 6,
     timeCN: '0025'
   },
+  415587: {
+    weekDayCN: 6,
+    timeCN: '0035'
+  },
+  349039: {
+    weekDayCN: 6,
+    timeCN: '0055'
+  },
+  387803: {
+    weekDayCN: 6,
+    timeCN: '0125'
+  },
   411247: {
     weekDayCN: 5,
-    timeCN: '1655'
+    timeCN: '1755'
   },
   422505: {
     weekDayCN: 6,
-    timeCN: '0500'
+    timeCN: '0600'
   },
   394784: {
     weekDayCN: 6,
-    timeCN: '1530'
+    timeCN: '1630'
   },
   404804: {
     weekDayCN: 6,
-    timeCN: '2000'
+    timeCN: '2100'
   },
   325808: {
     weekDayCN: 6,
-    timeCN: '2100'
+    timeCN: '2200'
   },
   395684: {
     weekDayCN: 6,
-    timeCN: '2230'
+    timeCN: '2330'
   },
   368289: {
     weekDayCN: 6,
-    timeCN: '2255'
+    timeCN: '2355'
   },
   394260: {
-    weekDayCN: 6,
-    timeCN: '2330'
+    weekDayCN: 7,
+    timeCN: '0030'
   },
   373833: {
     weekDayCN: 6,
-    timeCN: '2030'
+    timeCN: '2130'
   },
   379442: {
-    weekDayCN: 6,
-    timeCN: '2330'
+    weekDayCN: 7,
+    timeCN: '0030'
   },
   354421: {
     weekDayCN: 7,
-    timeCN: '0000'
+    timeCN: '0100'
   },
   413786: {
     weekDayCN: 7,
-    timeCN: '0700'
+    timeCN: '0800'
   },
   394746: {
-    weekDayCN: 7,
-    timeCN: '2300'
+    weekDayCN: 1,
+    timeCN: '0000'
   },
   408144: {
     weekDayCN: 7,
-    timeCN: '2000'
+    timeCN: '2100'
   },
   388067: {
     weekDayCN: 7,
-    timeCN: '2130'
+    timeCN: '2230'
   },
   403238: {
     weekDayCN: 7,
-    timeCN: '1500'
+    timeCN: '1600'
   },
   377769: {
     weekDayCN: 7,
-    timeCN: '1500'
+    timeCN: '1600'
   },
   409088: {
     weekDayCN: 7,
-    timeCN: '2100'
+    timeCN: '2200'
   },
   369768: {
     weekDayCN: 7,
-    timeCN: '2115'
+    timeCN: '2215'
   }
 } as const
