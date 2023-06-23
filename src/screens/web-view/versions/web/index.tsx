2dda33414f4bf73fe3398a34b9e9212fd7ddf62b
czy0729 <402731062@qq.com>
2023-06-24 07:12:04 +0800

    - publish v7.15.2

--- src/screens/web-view/versions/web/index.tsx
+++ src/screens/web-view/versions/web/index.tsx
@@ -2,7 +2,7 @@
  * @Author: czy0729
  * @Date: 2023-06-23 03:58:08
  * @Last Modified by: czy0729
- * @Last Modified time: 2023-06-23 06:16:20
+ * @Last Modified time: 2023-06-24 06:53:31
  */
 import React from 'react'
 import { Loading } from '@components'
@@ -34,8 +34,8 @@ function Web({ uri }) {
         source={{
           uri: `https://www.yuque.com/chenzhenyu-k0epm/znygb4/${uri}?singleDoc`
         }}
-        androidHardwareAccelerationDisabled
-        androidLayerType='software'
+        // androidHardwareAccelerationDisabled
+        // androidLayerType='software'
         javaScriptEnabled
         injectedJavaScript={injectedJavaScript()}
         bounces={false}
