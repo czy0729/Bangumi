cdf77ce2a7c33b6fd7f7808e4c7025d550ecc574
czy0729 <402731062@qq.com>
2023-10-30 22:56:47 +0800

    - fixed #121

--- src/screens/home/v2/item/progress/index.tsx
+++ src/screens/home/v2/item/progress/index.tsx
@@ -2,7 +2,7 @@
  * @Author: czy0729
  * @Date: 2021-01-21 16:01:56
  * @Last Modified by: czy0729
- * @Last Modified time: 2023-07-02 11:22:51
+ * @Last Modified time: 2023-10-30 22:46:43
  */
 import React from 'react'
 import { OnairProgress } from '@_'
@@ -12,12 +12,21 @@ import { Ctx } from '../../types'
 
 function Progress({ epStatus, subjectId }, { $ }: Ctx) {
   const { homeListCompact } = systemStore.setting
+  const total = $.epsCount(subjectId)
+  let current = $.currentOnAir(subjectId)
+
+  // 有一种情况为多季度番剧, 章节数非 0 或 1 开始的
+  // 会出现当前集数比总章节数多的情况, 需要使用实际放送章节数代替当前章节数
+  if (current > total) {
+    current = $.epsNoSp(subjectId).filter(item => item.status === 'Air').length
+  }
+
   return (
     <OnairProgress
       key={String($.$Item(subjectId).doing)}
       epStatus={epStatus}
-      current={$.currentOnAir(subjectId)}
-      total={$.epsCount(subjectId)}
+      total={total}
+      current={current}
       height={homeListCompact ? 5 : 6}
     />
   )
