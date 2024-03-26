0b8176598ae1fa8d5da1eaaef773eae030743475
czy0729 <402731062@qq.com>
2024-03-27 06:19:24 +0800

    - [空间] 修复了安卓环境下, 关于TA不能继续往下滑动的问题

--- src/screens/user/zone/component/about/index.android.tsx
+++ src/screens/user/zone/component/about/index.android.tsx
@@ -2,7 +2,7 @@
  * @Author: czy0729
  * @Date: 2024-01-07 17:32:28
  * @Last Modified by: czy0729
- * @Last Modified time: 2024-01-07 17:46:15
+ * @Last Modified time: 2024-03-27 06:17:26
  */
 import React from 'react'
 import { Animated } from 'react-native'
@@ -15,7 +15,11 @@ import { memoStyles } from './styles'
 function About() {
   const styles = memoStyles()
   return (
-    <Animated.ScrollView contentContainerStyle={styles.nestScroll} {...SCROLL_VIEW_RESET_PROPS}>
+    <Animated.ScrollView
+      nestedScrollEnabled
+      contentContainerStyle={styles.nestScroll}
+      {...SCROLL_VIEW_RESET_PROPS}
+    >
       <Content />
     </Animated.ScrollView>
   )
