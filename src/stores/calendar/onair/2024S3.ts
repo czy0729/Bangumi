b22f468e1d56881f3ea22ff89555f8272a2aaaa2
czy0729 <402731062@qq.com>
2024-07-01 06:57:50 +0800

    - [每日放送] 更新 2024S3 番剧放送时间

--- src/stores/calendar/onair/2024S3.ts
+++ src/stores/calendar/onair/2024S3.ts
@@ -2,13 +2,13 @@
  * @Author: czy0729
  * @Date: 2024-06-19 02:05:26
  * @Last Modified by: czy0729
- * @Last Modified time: 2024-06-19 02:06:11
+ * @Last Modified time: 2024-07-01 06:39:39
  */
 export const ONAIR_2024S3 = {
   /** 少女如草花绽放 */
   464561: {
     weekDayCN: 7,
-    timeCN: '',
+    timeCN: '2245',
     type: '原创',
     tag: '啦啦队/竞技/贴贴',
     origin: 'P.A.WORKS'
@@ -17,7 +17,7 @@ export const ONAIR_2024S3 = {
   /** 深夜冲击 */
   477207: {
     weekDayCN: 1,
-    timeCN: '',
+    timeCN: '2130',
     type: '原创',
     tag: '视频创作/贴贴',
     origin: 'P.A.WORKS'
@@ -25,8 +25,8 @@ export const ONAIR_2024S3 = {
 
   /** 异世界自杀小队​​ */
   444046: {
-    weekDayCN: 5,
-    timeCN: '',
+    weekDayCN: 4,
+    timeCN: '1501',
     type: '原创',
     tag: '穿越/奇幻/暴力',
     origin: 'WIT Studio'
@@ -35,7 +35,7 @@ export const ONAIR_2024S3 = {
   /** 战国妖狐 千魔混沌篇 */
   488197: {
     weekDayCN: 3,
-    timeCN: '',
+    timeCN: '2300',
     type: '漫画改',
     tag: '妖魔/冒险/战斗',
     origin: 'WHITE FOX'
@@ -53,7 +53,7 @@ export const ONAIR_2024S3 = {
   /** 【我推的孩子】 第二季 */
   443428: {
     weekDayCN: 3,
-    timeCN: '',
+    timeCN: '2200',
     type: '漫画改',
     tag: '演艺界/扭曲/兄妹',
     origin: '动画工房'
@@ -62,7 +62,7 @@ export const ONAIR_2024S3 = {
   /** 女神的露天咖啡厅 第二季 */
   442673: {
     weekDayCN: 4,
-    timeCN: '',
+    timeCN: '2326',
     type: '漫画改',
     tag: '恋爱/喜剧/后宫',
     origin: '手冢Production'
@@ -71,7 +71,7 @@ export const ONAIR_2024S3 = {
   /** 腼腆英雄 东京夺还篇 */
   470874: {
     weekDayCN: 1,
-    timeCN: '',
+    timeCN: '2300',
     type: '漫画改',
     tag: '超级英雄/战斗',
     origin: '8bit'
@@ -80,7 +80,7 @@ export const ONAIR_2024S3 = {
   /** 筋肉人 完美超人始祖篇 */
   424853: {
     weekDayCN: 7,
-    timeCN: '',
+    timeCN: '2230',
     type: '漫画改',
     tag: '超级英雄/战斗',
     origin: 'Production I.G'
@@ -89,7 +89,7 @@ export const ONAIR_2024S3 = {
   /** 妖精的尾巴 百年任务 */
   349093: {
     weekDayCN: 7,
-    timeCN: '',
+    timeCN: '1630',
     type: '漫画改',
     tag: '冒险/战斗/热血',
     origin: 'J.C.STAFF'
@@ -97,8 +97,8 @@ export const ONAIR_2024S3 = {
 
   /** 古连泰沙U */
   448613: {
-    weekDayCN: 5,
-    timeCN: '',
+    weekDayCN: 6,
+    timeCN: '0023',
     type: '漫画改',
     tag: '科幻/机战/超级系',
     origin: 'GAINA'
@@ -107,7 +107,7 @@ export const ONAIR_2024S3 = {
   /** 神之塔 第二季 */
   394996: {
     weekDayCN: 7,
-    timeCN: '',
+    timeCN: '2200',
     type: '漫画改',
     tag: '冒险/爬塔/战斗',
     origin: 'Answer Studio'
@@ -134,7 +134,7 @@ export const ONAIR_2024S3 = {
   /** 拉面红猫 */
   467243: {
     weekDayCN: 4,
-    timeCN: '',
+    timeCN: '2256',
     type: '漫画改',
     tag: '喵星人/美食/日常',
     origin: 'E&H production'
@@ -143,7 +143,7 @@ export const ONAIR_2024S3 = {
   /** 擅长逃跑的殿下 */
   424573: {
     weekDayCN: 6,
-    timeCN: '',
+    timeCN: '2230',
     type: '漫画改',
     tag: '历史/逃亡/复仇',
     origin: 'CloverWorks'
@@ -151,8 +151,8 @@ export const ONAIR_2024S3 = {
 
   /** 地下城里的人们 */
   450586: {
-    weekDayCN: 5,
-    timeCN: '',
+    weekDayCN: 6,
+    timeCN: '0053',
     type: '漫画改',
     tag: '职场/奇妙日常',
     origin: 'OLM'
@@ -161,7 +161,7 @@ export const ONAIR_2024S3 = {
   /** 杖与剑的魔剑谭 */
   479477: {
     weekDayCN: 7,
-    timeCN: '',
+    timeCN: '1530',
     type: '漫画改',
     tag: '魔法学园/战斗',
     origin: 'Actas/BN Pictures'
@@ -169,8 +169,8 @@ export const ONAIR_2024S3 = {
 
   /** 这个世界漏洞百出 */
   425890: {
-    weekDayCN: 5,
-    timeCN: '',
+    weekDayCN: 6,
+    timeCN: '0123',
     type: '漫画改',
     tag: '游戏世界/debug',
     origin: '100studio/Studio Pallet'
@@ -179,7 +179,7 @@ export const ONAIR_2024S3 = {
   /** 异世界失格 */
   391496: {
     weekDayCN: 2,
-    timeCN: '',
+    timeCN: '2230',
     type: '漫画改',
     tag: '穿越/奇妙冒险',
     origin: 'Atelier Pontdarc'
@@ -187,8 +187,8 @@ export const ONAIR_2024S3 = {
 
   /** 精灵小姐瘦不了。 */
   475367: {
-    weekDayCN: 6,
-    timeCN: '',
+    weekDayCN: 7,
+    timeCN: '0030',
     type: '漫画改',
     tag: '逆穿越/减肥/涩涩',
     origin: 'Elias'
@@ -197,7 +197,7 @@ export const ONAIR_2024S3 = {
   /** 鹿乃子乃子乃子虎视眈眈 */
   484761: {
     weekDayCN: 7,
-    timeCN: '',
+    timeCN: '2230',
     type: '漫画改',
     tag: '校园/抽象/无厘头',
     origin: 'WIT Studio'
@@ -206,7 +206,7 @@ export const ONAIR_2024S3 = {
   /** 魔法少女与恶曾是敌人。 */
   465884: {
     weekDayCN: 2,
-    timeCN: '',
+    timeCN: '2145',
     type: '漫画改',
     tag: '敌对恋/狗粮/宠溺',
     origin: 'BONES'
@@ -215,7 +215,7 @@ export const ONAIR_2024S3 = {
   /** 2.5次元的诱惑 */
   410346: {
     weekDayCN: 5,
-    timeCN: '',
+    timeCN: '2130',
     type: '漫画改',
     tag: 'Cos/热血/涩涩',
     origin: 'J.C.STAFF'
@@ -224,7 +224,7 @@ export const ONAIR_2024S3 = {
   /** 疑似后宫 */
   428823: {
     weekDayCN: 4,
-    timeCN: '',
+    timeCN: '2330',
     type: '漫画改',
     tag: '校园/恋爱喜剧',
     origin: 'NOMAD'
@@ -232,8 +232,8 @@ export const ONAIR_2024S3 = {
 
   /** 我的妻子没有感情 */
   482776: {
-    weekDayCN: 2,
-    timeCN: '',
+    weekDayCN: 6,
+    timeCN: '2200',
     type: '漫画改',
     tag: '人机恋/日常/温馨',
     origin: '手冢Production'
@@ -242,7 +242,7 @@ export const ONAIR_2024S3 = {
   /** 前辈是男孩子 */
   425988: {
     weekDayCN: 4,
-    timeCN: '',
+    timeCN: '2355',
     type: '漫画改',
     tag: '女装/恋爱/友情',
     origin: 'project No.9'
@@ -251,7 +251,7 @@ export const ONAIR_2024S3 = {
   /** 黄昏失焦 */
   443931: {
     weekDayCN: 4,
-    timeCN: '',
+    timeCN: '2230',
     type: '漫画改',
     tag: '社团/摄影/BL',
     origin: 'Studio DEEN'
@@ -259,14 +259,14 @@ export const ONAIR_2024S3 = {
 
   /** 多数欠 */
   477204: {
-    weekDayCN: 2,
-    timeCN: '',
+    weekDayCN: 3,
+    timeCN: '0059',
     type: '漫画改',
     tag: '死亡游戏/智斗',
     origin: 'Satelight'
   },
 
-  /** 高尔夫物语  */
+  /** 高尔夫物语 */
   469400: {
     weekDayCN: 6,
     timeCN: '',
@@ -278,7 +278,7 @@ export const ONAIR_2024S3 = {
   /** 物语系列 外传季&怪物季 */
   475354: {
     weekDayCN: 6,
-    timeCN: '',
+    timeCN: '2100',
     type: '小说改',
     tag: '怪异/群像/外传',
     origin: 'SHAFT'
@@ -287,7 +287,7 @@ export const ONAIR_2024S3 = {
   /** 这是你与我的最后战场，或是开创世界的圣战 第二季 */
   351349: {
     weekDayCN: 3,
-    timeCN: '',
+    timeCN: '2300',
     type: '小说改',
     tag: '魔法科技/敌对恋',
     origin: 'Studio Pallet/SILVER LINK.'
@@ -296,7 +296,7 @@ export const ONAIR_2024S3 = {
   /** 为何我的世界被遗忘了？ */
   446615: {
     weekDayCN: 6,
-    timeCN: '',
+    timeCN: '2130',
     type: '小说改',
     tag: '奇幻/战斗/恋爱',
     origin: 'project No.9'
@@ -305,7 +305,7 @@ export const ONAIR_2024S3 = {
   /** 再见，地球 */
   408195: {
     weekDayCN: 5,
-    timeCN: '',
+    timeCN: '2230',
     type: '小说改',
     tag: '奇幻/冒险/战斗',
     origin: 'LIDENFILMS'
@@ -313,8 +313,8 @@ export const ONAIR_2024S3 = {
 
   /** 小市民系列 */
   474906: {
-    weekDayCN: 6,
-    timeCN: '',
+    weekDayCN: 7,
+    timeCN: '0030',
     type: '小说改',
     tag: '推理/日常/甜点',
     origin: 'Lapin Track'
@@ -323,7 +323,7 @@ export const ONAIR_2024S3 = {
   /** 败犬女主太多了！ */
   464376: {
     weekDayCN: 6,
-    timeCN: '',
+    timeCN: '2330',
     type: '小说改',
     tag: '恋爱/喜剧',
     origin: 'A-1 Pictures'
@@ -332,7 +332,7 @@ export const ONAIR_2024S3 = {
   /** 不时轻声地以俄语遮羞的邻座艾莉同学 */
   424883: {
     weekDayCN: 3,
-    timeCN: '',
+    timeCN: '2230',
     type: '小说改',
     tag: '校园/恋爱喜剧',
     origin: '动画工房'
@@ -341,7 +341,7 @@ export const ONAIR_2024S3 = {
   /** 义妹生活 */
   393037: {
     weekDayCN: 4,
-    timeCN: '',
+    timeCN: '2000',
     type: '小说改',
     tag: '兄妹/家庭/恋爱',
     origin: 'Studio DEEN'
@@ -350,7 +350,7 @@ export const ONAIR_2024S3 = {
   /** 恋爱没法用双子除尽 */
   445722: {
     weekDayCN: 3,
-    timeCN: '',
+    timeCN: '2230',
     type: '小说改',
     tag: '双子三角恋/胃疼',
     origin: 'ROLL2'
@@ -359,7 +359,7 @@ export const ONAIR_2024S3 = {
   /** 身为VTuber的我因为忘记关台而成了传说 */
   416320: {
     weekDayCN: 7,
-    timeCN: '',
+    timeCN: '2230',
     type: '小说改',
     tag: 'VTB/反差/贴贴',
     origin: 'TNK'
@@ -368,7 +368,7 @@ export const ONAIR_2024S3 = {
   /** 魔导具师妲莉亚永不妥协 */
   446175: {
     weekDayCN: 6,
-    timeCN: '',
+    timeCN: '2000',
     type: '小说改',
     tag: '转生/大女主/奋斗',
     origin: '台风Graphics/Imagica Infos'
@@ -376,8 +376,8 @@ export const ONAIR_2024S3 = {
 
   /** 异世界悠闲纪行 ～边养娃边当冒险者～ */
   467811: {
-    weekDayCN: 7,
-    timeCN: '',
+    weekDayCN: 1,
+    timeCN: '0050',
     type: '小说改',
     tag: '转生/冒险/带娃',
     origin: 'EMT Squared'
@@ -386,7 +386,7 @@ export const ONAIR_2024S3 = {
   /** 魔王军最强的魔术师是人类 */
   475933: {
     weekDayCN: 3,
-    timeCN: '',
+    timeCN: '2130',
     type: '小说改',
     tag: '转生/魔法/战争',
     origin: 'studio A-CAT'
@@ -395,7 +395,7 @@ export const ONAIR_2024S3 = {
   /** 依靠被嫌弃的【状态异常技能】而成为最强的我最终蹂躏一切 */
   476418: {
     weekDayCN: 4,
-    timeCN: '',
+    timeCN: '2359',
     type: '小说改',
     tag: '穿越/冒险/复仇',
     origin: 'Seven Arcs'
@@ -404,7 +404,7 @@ export const ONAIR_2024S3 = {
   /** 我要【招架】一切〜反误解的世界最强想成为冒险者〜 */
   463389: {
     weekDayCN: 4,
-    timeCN: '',
+    timeCN: '2300',
     type: '小说改',
     tag: '冒险/战斗/喜剧',
     origin: 'OLM'
@@ -412,8 +412,8 @@ export const ONAIR_2024S3 = {
 
   /** 大叔新人冒险者被最强小队拼死锻炼后无敌了。 */
   427059: {
-    weekDayCN: 1,
-    timeCN: '',
+    weekDayCN: 2,
+    timeCN: '0100',
     type: '小说改',
     tag: '冒险/战斗/喜剧',
     origin: '梦太Company'
@@ -422,7 +422,7 @@ export const ONAIR_2024S3 = {
   /** 从路人角色开始的探索英雄谭 */
   472554: {
     weekDayCN: 6,
-    timeCN: '',
+    timeCN: '2100',
     type: '小说改',
     tag: '现代奇幻/战斗',
     origin: '月虹'
@@ -431,7 +431,7 @@ export const ONAIR_2024S3 = {
   /** 尼尔：自动人形 Ver1.1a 第2部分 */
   446618: {
     weekDayCN: 5,
-    timeCN: '',
+    timeCN: '2230',
     type: '游戏改',
     tag: '科幻/末世/战争',
     origin: 'A-1 Pictures'
@@ -440,7 +440,7 @@ export const ONAIR_2024S3 = {
   /** 亚托莉 -我挚爱的时光- */
   397604: {
     weekDayCN: 6,
-    timeCN: '',
+    timeCN: '2300',
     type: '游戏改',
     tag: '仿生机器人/恋爱',
     origin: 'TROYCA'
@@ -449,7 +449,7 @@ export const ONAIR_2024S3 = {
   /** 天穗之咲稻姬 */
   478684: {
     weekDayCN: 6,
-    timeCN: '',
+    timeCN: '2200',
     type: '游戏改',
     tag: '神魔/种田/冒险',
     origin: 'P.A.WORKS'
@@ -458,7 +458,7 @@ export const ONAIR_2024S3 = {
   /** 德里科的托儿所 */
   448879: {
     weekDayCN: 3,
-    timeCN: '',
+    timeCN: '2330',
     type: '其他',
     tag: '吸血鬼/悬疑/带娃',
     origin: 'J.C.STAFF'
