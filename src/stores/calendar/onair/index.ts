ee53fc7da6ac2f685c296d2718026426592dea45
czy0729 <402731062@qq.com>
2024-10-04 04:42:21 +0800

    - 补充 2024S4 番剧数据

--- src/stores/calendar/onair/index.ts
+++ src/stores/calendar/onair/index.ts
@@ -2,14 +2,16 @@
  * @Author: czy0729
  * @Date: 2023-04-06 05:46:03
  * @Last Modified by: czy0729
- * @Last Modified time: 2024-03-28 13:57:28
+ * @Last Modified time: 2024-10-04 04:23:15
  */
-import { ONAIR_2024S1 } from './2024S1'
-import { ONAIR_2024S2 } from './2024S2'
+// import { ONAIR_2024S1 } from './2024S1'
+// import { ONAIR_2024S2 } from './2024S2'
 import { ONAIR_2024S3 } from './2024S3'
+import { ONAIR_2024S4 } from './2024S4'
 
 export const ON_AIR = {
-  ...ONAIR_2024S1,
-  ...ONAIR_2024S2,
-  ...ONAIR_2024S3
+  // ...ONAIR_2024S1,
+  // ...ONAIR_2024S2,
+  ...ONAIR_2024S3,
+  ...ONAIR_2024S4
 } as const
