20e91f2fae75180ebc1c895ace4ceaa71ab616cc
czy0729 <402731062@qq.com>
2024-11-30 15:57:51 +0800

    - [iOS] 修复 GestureDetector

---
+++ src/screens/_/base/tap-listener/index.old.tsx
@@ -0,0 +1,45 @@
+/*
+ * @Author: czy0729
+ * @Date: 2022-08-13 09:58:37
+ * @Last Modified by: czy0729
+ * @Last Modified time: 2024-11-15 15:06:01
+ */
+import React from 'react'
+import {
+  PanGestureHandler,
+  PanGestureHandlerStateChangeEvent,
+  State
+} from 'react-native-gesture-handler'
+import { _, uiStore } from '@stores'
+import { ob } from '@utils/decorators'
+import { COMPONENT } from './ds'
+
+/**
+ * 获取手指最近一次点击相对于屏幕的坐标
+ * @Doc https://docs.swmansion.com/react-native-gesture-handler/docs/2.1.1/api/gestures/pan-gesture
+ */
+export const TapListener = ob(
+  ({ children }) => (
+    <PanGestureHandler
+      enabled
+      // 设置下面值, 使得这个容器不应该能激活手势, 只用来获取点击 x, y 坐标
+      minPointers={1}
+      maxPointers={1}
+      activeOffsetX={_.window.contentWidth}
+      activeOffsetY={_.window.contentWidth}
+      onHandlerStateChange={(event: PanGestureHandlerStateChangeEvent) => {
+        if (event.nativeEvent.state === State.FAILED) {
+          uiStore.setXY(
+            Math.floor(event.nativeEvent.absoluteX),
+            Math.floor(event.nativeEvent.absoluteY)
+          )
+        }
+      }}
+    >
+      {children}
+    </PanGestureHandler>
+  ),
+  COMPONENT
+)
+
+export default TapListener
