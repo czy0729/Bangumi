37eecd6054a44f636a993f4a308f633a8711833e
czy0729 <402731062@qq.com>
2025-08-21 17:45:25 +0800

    - [iOS] 搁置 ImageViewer 在初始 index 非 0 时布局错误的问题

--- src/components/image-viewer/index.tsx
+++ src/components/image-viewer/index.tsx
@@ -2,11 +2,11 @@
  * @Author: czy0729
  * @Date: 2019-05-23 18:57:26
  * @Last Modified by: czy0729
- * @Last Modified time: 2025-06-10 17:22:51
+ * @Last Modified time: 2025-08-21 17:43:17
  */
-import React from 'react'
+import React, { useCallback, useMemo } from 'react'
 import { Modal, View } from 'react-native'
-import { observer } from 'mobx-react'
+import { useObserver } from 'mobx-react'
 import ActivityIndicator from '@ant-design/react-native/lib/activity-indicator'
 import { open, showActionSheet, stl } from '@utils'
 import { r } from '@utils/dev'
@@ -27,117 +27,120 @@ export { ImageViewerProps }
  * 图片相册查看器
  * @doc https://github.com/ascoders/react-native-image-viewer
  */
-export const ImageViewer = observer(
-  class ImageViewerComponent extends React.Component<ImageViewerProps> {
-    static defaultProps: ImageViewerProps = {
-      index: 0,
-      visible: false,
-      imageUrls: [],
-      mini: false,
-      useRN: false,
-      onCancel: FROZEN_FN
+export const ImageViewer = ({
+  visible = false,
+  index = 0,
+  imageUrls = [],
+  mini = false,
+  useRN = false,
+  onCancel = FROZEN_FN,
+  ...other
+}: ImageViewerProps) => {
+  r(COMPONENT)
+
+  // 组件 RNImageViewer 在 iOS Expo 环境下代码已过时, 导致初始 index 非 0 时布局错乱 (待处理)
+  const iOSToFixed = IOS && index !== 0
+  const memoData = useMemo(
+    () => (iOSToFixed ? [imageUrls[index]] : imageUrls),
+    [iOSToFixed, imageUrls, index]
+  )
+  const selectedIndex = iOSToFixed ? 0 : index
+
+  const handleRequestClose = useCallback(() => {
+    if (typeof onCancel === 'function') onCancel()
+  }, [onCancel])
+
+  const handleRenderMenus = useCallback((url: string, cancel: any) => {
+    if (typeof url === 'string' && url.includes(HOST_DOGE)) return null
+
+    if (IOS) {
+      // 不想涉及到权限问题, 暂时用浏览器打开图片来处理
+      showActionSheet(ACTION_SHEET_DS, (i: number) => {
+        if (i === 0) open(url)
+      })
+    } else {
+      // @issue 安卓的 ActionSheet 在这个 Viewer 的下面
+      cancel?.()
+      showActionSheet(ACTION_SHEET_DS, (i: number) => {
+        if (i === 0) open(url)
+      })
     }
+    return null
+  }, [])
 
-    onRequestClose = () => {
-      const { onCancel } = this.props
-      if (typeof onCancel === 'function') onCancel()
-    }
+  const handleMenus = useCallback(() => {
+    const currentUrl = memoData[selectedIndex]?._url || memoData[selectedIndex]?.url
+    return handleRenderMenus(currentUrl, onCancel)
+  }, [memoData, selectedIndex, handleRenderMenus, onCancel])
 
-    onMenus = () => {
-      const { index, imageUrls, onCancel } = this.props
-      return this.renderMenus(imageUrls[index]._url || imageUrls[index].url, onCancel)
-    }
-
-    renderMenus(url: string, onCancel: any) {
-      if (typeof url === 'string' && url.includes(HOST_DOGE)) return
-
-      if (IOS) {
-        // 不想涉及到权限问题, 暂时用浏览器打开图片来处理
-        showActionSheet(ACTION_SHEET_DS, (index: number) => {
-          if (index === 0) open(url)
-        })
-      } else {
-        // @issue 安卓的 ActionSheet 在这个 Viewer 的下面
-        onCancel()
-        showActionSheet(ACTION_SHEET_DS, (index: number) => {
-          if (index === 0) open(url)
-        })
-      }
-
-      return null
-    }
-
-    renderIndicator = (currentIndex: number, allSize: number) => {
-      const { imageUrls } = this.props
-      if (imageUrls.length <= 1) return null
+  const handleRenderIndicator = useCallback(
+    (currentIndex: number, allSize: number) => {
+      if (memoData.length <= 1) return null
 
       return (
         <Text style={styles.indicator} type='__plain__' align='center' pointerEvents='none'>
           {currentIndex} / {allSize}
         </Text>
       )
-    }
+    },
+    [memoData.length]
+  )
 
-    renderImage = (props: any) => {
-      if (!(props?.style?.width || props?.style?.height)) return null
+  const handleRenderImage = useCallback(
+    (p: any) => {
+      if (!(p?.style?.width || p?.style?.height)) return null
 
-      const { imageUrls } = this.props
       return (
         <Image
-          src={props?.source?.uri}
-          width={props?.style?.width}
-          height={props?.style?.height}
-          headers={imageUrls?.[0]?.headers}
+          src={p?.source?.uri}
+          width={p?.style?.width}
+          height={p?.style?.height}
+          headers={memoData?.[0]?.headers}
           placeholder={false}
           skeleton={false}
         />
       )
-    }
+    },
+    [memoData]
+  )
 
-    render() {
-      r(COMPONENT)
-
-      const { index, visible, imageUrls, mini, useRN, onCancel, ...other } = this.props
-      return (
-        <Component id='component-image-viewer'>
-          <Modal
-            visible={visible}
-            transparent
-            hardwareAccelerated={false}
-            animationType='fade'
-            statusBarTranslucent
-            onRequestClose={this.onRequestClose}
-          >
-            <View style={styles.container}>
-              <View style={styles.activityIndicator}>
-                <ActivityIndicator />
-              </View>
-              <View style={stl(styles.viewerContainer, mini && styles.viewerMini)}>
-                <RNImageViewer
-                  style={styles.viewer}
-                  // tofix
-                  index={IOS ? 0 : index}
-                  imageUrls={imageUrls}
-                  backgroundColor='transparent'
-                  enableSwipeDown={!mini}
-                  enableImageZoom={!mini}
-                  menus={this.onMenus}
-                  saveToLocalByLongPress={false}
-                  renderIndicator={this.renderIndicator}
-                  renderImage={useRN ? undefined : this.renderImage}
-                  onCancel={onCancel}
-                  {...other}
-                />
-              </View>
-              <Touchable style={styles.close} useRN onPress={onCancel}>
-                <Iconfont style={styles.iconfont} name='md-close' />
-              </Touchable>
-            </View>
-          </Modal>
-        </Component>
-      )
-    }
-  }
-)
+  return useObserver(() => (
+    <Component id='component-image-viewer'>
+      <Modal
+        visible={visible}
+        transparent
+        hardwareAccelerated={false}
+        animationType='fade'
+        statusBarTranslucent
+        onRequestClose={handleRequestClose}
+      >
+        <View style={styles.container}>
+          <View style={styles.activityIndicator}>
+            <ActivityIndicator />
+          </View>
+          <View style={stl(styles.viewerContainer, mini && styles.viewerMini)}>
+            <RNImageViewer
+              style={styles.viewer}
+              index={selectedIndex}
+              imageUrls={memoData}
+              backgroundColor='transparent'
+              enableSwipeDown={!mini}
+              enableImageZoom={!mini}
+              menus={handleMenus}
+              saveToLocalByLongPress={false}
+              renderIndicator={handleRenderIndicator}
+              renderImage={useRN ? undefined : handleRenderImage}
+              onCancel={onCancel}
+              {...other}
+            />
+          </View>
+          <Touchable style={styles.close} useRN onPress={onCancel}>
+            <Iconfont style={styles.iconfont} name='md-close' />
+          </Touchable>
+        </View>
+      </Modal>
+    </Component>
+  ))
+}
 
 export default ImageViewer
