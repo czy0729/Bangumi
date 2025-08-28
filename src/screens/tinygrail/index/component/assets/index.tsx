85a812bab2bcb785155baf596f468775550c59d7
czy0729 <402731062@qq.com>
2025-08-29 06:12:57 +0800

    - [小圣杯] 修复负资产数字没有正确缩略的问题

--- src/screens/tinygrail/index/component/assets/index.tsx
+++ src/screens/tinygrail/index/component/assets/index.tsx
@@ -2,121 +2,80 @@
  * @Author: czy0729
  * @Date: 2019-11-17 01:37:57
  * @Last Modified by: czy0729
- * @Last Modified time: 2024-11-19 06:18:07
+ * @Last Modified time: 2025-08-29 06:01:09
  */
 import React from 'react'
-import { Flex, Text, TextType, Touchable } from '@components'
+import { Flex, Text, Touchable } from '@components'
 import { IconTouchable } from '@_'
 import { _, useStore } from '@stores'
-import { formatNumber, toFixed } from '@utils'
-import { ob } from '@utils/decorators'
-import { M } from '@constants'
+import { r } from '@utils/dev'
+import { useObserver } from '@utils/hooks'
 import { Ctx } from '../../types'
+import { formatChange, formatValue } from './utils'
 import { COMPONENT } from './ds'
 import { memoStyles } from './styles'
 
 function Assets() {
+  r(COMPONENT)
+
   const { $ } = useStore<Ctx>()
-  const styles = memoStyles()
-  const { currentBalance, currentTotal, lastBalance, lastTotal } = $.state
-  const { balance, lastIndex } = $.assets
 
-  // 缩短
-  let _balance: any = balance
-  let _total: any = $.total
-  if ($.short) {
-    if (_balance > 1000) {
-      _balance = `${toFixed(_balance / M, 1)}万`
-    } else {
-      _balance = formatNumber(_balance, 1)
-    }
+  return useObserver(() => {
+    const styles = memoStyles()
+    const { currentBalance, currentTotal, lastBalance, lastTotal } = $.state
+    const { balance, lastIndex } = $.assets
 
-    if (_total > 1000) {
-      _total = `${toFixed(_total / M, 1)}万`
-    } else {
-      _total = formatNumber(_total, 1)
-    }
-  } else {
-    _balance = formatNumber(_balance, 1)
-    _total = formatNumber(_total, 1)
-  }
+    // 数值
+    const balanceText = formatValue(balance, $.short)
+    const totalText = formatValue($.total, $.short)
 
-  // 变化
-  const changeBalance = currentBalance - lastBalance
-  const changeTotal = currentTotal - lastTotal
+    // 变化
+    const { text: balanceChange, color: balanceChangeColor } = formatChange(
+      currentBalance,
+      lastBalance,
+      $.short
+    )
+    const { text: totalChange, color: totalChangeColor } = formatChange(
+      currentTotal,
+      lastTotal,
+      $.short
+    )
 
-  let balanceChangeText: string
-  let balanceTextColor: TextType
-  let _changeBalance: string
-  if ($.short && Math.abs(changeBalance) >= 1000) {
-    _changeBalance = `${toFixed(Math.abs(changeBalance) / M, 1)}万`
-  } else {
-    _changeBalance = formatNumber(Math.abs(changeBalance), 1)
-  }
-
-  if (changeBalance > 0) {
-    balanceChangeText = `+${_changeBalance}`
-    balanceTextColor = 'bid'
-  } else if (changeBalance < 0) {
-    balanceChangeText = `-${_changeBalance}`
-    balanceTextColor = 'ask'
-  } else {
-    balanceChangeText = ''
-  }
-
-  let totalChangeText: string
-  let totalTextColor: TextType
-  let _changeTotal: string
-  if ($.short && Math.abs(changeTotal) >= 1000) {
-    _changeTotal = `${toFixed(Math.abs(changeTotal) / M, 1)}万`
-  } else {
-    _changeTotal = formatNumber(Math.abs(changeTotal), 1)
-  }
-
-  if (changeTotal > 0) {
-    totalChangeText = `+${_changeTotal}`
-    totalTextColor = 'bid'
-  } else if (changeTotal < 0) {
-    totalChangeText = `-${_changeTotal}`
-    totalTextColor = 'ask'
-  } else {
-    totalChangeText = ''
-  }
-
-  return (
-    <Flex style={styles.assets}>
-      <Flex.Item>
-        <Touchable style={styles.touch} onPress={$.toogleShort}>
-          <Text type='tinygrailPlain' size={13} bold>
-            {_balance}
-            {balanceChangeText && (
-              <Text type={balanceTextColor} size={10} lineHeight={13} bold>
-                {' '}
-                {balanceChangeText}
-              </Text>
-            )}{' '}
-            / {_total}{' '}
-            {totalChangeText && (
-              <Text type={totalTextColor} size={10} lineHeight={13} bold>
-                {totalChangeText}{' '}
-              </Text>
-            )}
-            {!!lastIndex && `/ #${lastIndex}`}
+    return (
+      <Flex style={styles.assets}>
+        <Flex.Item>
+          <Touchable style={styles.touch} onPress={$.toogleShort}>
             <Text type='tinygrailPlain' size={13} bold>
-              {' '}
-              {$.short ? '[-]' : '[+]'}
+              {balanceText}
+              {balanceChange && (
+                <Text type={balanceChangeColor} size={10} lineHeight={13} bold>
+                  {' '}
+                  {balanceChange}
+                </Text>
+              )}{' '}
+              / {totalText}{' '}
+              {totalChange && (
+                <Text type={totalChangeColor} size={10} lineHeight={13} bold>
+                  {totalChange}{' '}
+                </Text>
+              )}
+              {!!lastIndex && `/ #${lastIndex}`}
+              <Text type='tinygrailPlain' size={13} bold>
+                {' '}
+                {$.short ? '[-]' : '[+]'}
+              </Text>
             </Text>
-          </Text>
-        </Touchable>
-      </Flex.Item>
-      <IconTouchable
-        style={_.mr._sm}
-        name='md-calculate'
-        color={_.colorTinygrailPlain}
-        onPress={$.doTest}
-      />
-    </Flex>
-  )
+          </Touchable>
+        </Flex.Item>
+        <IconTouchable
+          style={_.mr._sm}
+          name='md-calculate'
+          color={_.colorTinygrailPlain}
+          onPress={$.doTest}
+        />
+      </Flex>
+    )
+  })
 }
 
-export default ob(Assets, COMPONENT)
+export default Assets
---
+++ src/screens/tinygrail/index/component/assets/utils.ts
@@ -0,0 +1,33 @@
+/*
+ * @Author: czy0729
+ * @Date: 2025-08-29 05:58:58
+ * @Last Modified by: czy0729
+ * @Last Modified time: 2025-08-29 06:09:58
+ */
+import { TextType } from '@components'
+import { formatNumber, toFixed } from '@utils'
+import { M } from '@constants'
+
+export function formatValue(value: number, short: boolean): string {
+  const isNegative = value < 0
+  const abs = Math.abs(value)
+  const body = short && abs >= 1000 ? `${toFixed(abs / M, 1)}万` : formatNumber(abs, 1)
+  return isNegative && abs !== 0 ? `-${body}` : body
+}
+
+export function formatChange(current: number, last: number, short: boolean) {
+  const diff = current - last
+  if (diff === 0) {
+    return {
+      text: '',
+      color: undefined as TextType | undefined
+    }
+  }
+
+  const abs = Math.abs(diff)
+  const formatted = short && abs >= 1000 ? `${toFixed(abs / M, 1)}万` : formatNumber(abs, 1)
+  return {
+    text: `${diff > 0 ? '+' : '-'}${formatted}`,
+    color: diff > 0 ? 'bid' : ('ask' as TextType)
+  }
+}
