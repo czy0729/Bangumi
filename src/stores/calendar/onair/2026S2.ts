d437ebe71d385339774a91540da702ce0af2cdc3
czy0729 <402731062@qq.com>
2026-03-30 23:00:50 +0800

    - [每日放送] 更新 2026S2 元数据

--- src/stores/calendar/onair/2026S2.ts
+++ src/stores/calendar/onair/2026S2.ts
@@ -1,14 +1,14 @@
 /*
  * @Author: czy0729
  * @Date: 2026-03-21 01:12:16
- * @Last Modified by:   czy0729
- * @Last Modified time: 2026-03-21 01:12:16
+ * @Last Modified by: czy0729
+ * @Last Modified time: 2026-03-30 22:43:12
  */
 export const ONAIR_2026S2 = {
   600675: {
     origin: 'ENGI',
     tag: '灵异/音乐/战斗',
-    timeCN: '',
+    timeCN: '2330',
     title: '幽灵音乐会 missing Songs',
     type: '原创动画',
     weekDayCN: 7
@@ -17,7 +17,7 @@ export const ONAIR_2026S2 = {
   501796: {
     origin: 'StudioPIERROT',
     tag: '魔法/变身/偶像',
-    timeCN: '',
+    timeCN: '2130',
     title: '魔法姐妹露露与莉莉',
     type: '原创动画',
     weekDayCN: 7
@@ -26,7 +26,7 @@ export const ONAIR_2026S2 = {
   622634: {
     origin: 'OLM/同友A＆E',
     tag: '音乐/偶像/变身',
-    timeCN: '',
+    timeCN: '0830',
     title: '拜托了偶像公主',
     type: '原创动画',
     weekDayCN: 7
@@ -35,7 +35,7 @@ export const ONAIR_2026S2 = {
   508004: {
     origin: 'TMS娱乐',
     tag: '奇科幻/冒险',
-    timeCN: '',
+    timeCN: '2100',
     title: '石纪元 第4期 Part.3',
     type: '漫画改编动画',
     weekDayCN: 4
@@ -44,7 +44,7 @@ export const ONAIR_2026S2 = {
   524486: {
     origin: 'BN Pictures',
     tag: '魔界/校园/喜剧',
-    timeCN: '',
+    timeCN: '1725',
     title: '入间同学入魔了 第4期',
     type: '漫画改编动画',
     weekDayCN: 6
@@ -53,7 +53,7 @@ export const ONAIR_2026S2 = {
   485990: {
     origin: 'Orange',
     tag: '架空/拟人/恋爱',
-    timeCN: '',
+    timeCN: '2355',
     title: '动物狂想曲 最终章 Part.2',
     type: '漫画改编动画',
     weekDayCN: 6
@@ -62,7 +62,7 @@ export const ONAIR_2026S2 = {
   551918: {
     origin: 'david production',
     tag: '奇幻/战斗/冒险',
-    timeCN: '',
+    timeCN: '2355',
     title: 'JOJO奇妙冒险 飙马野郎',
     type: '漫画改编动画',
     weekDayCN: 4
@@ -71,7 +71,7 @@ export const ONAIR_2026S2 = {
   495238: {
     origin: 'OLM',
     tag: '运动/棒球/热血',
-    timeCN: '',
+    timeCN: '1630',
     title: '钻石王牌 第4期 (actⅡ 第2期)',
     type: '漫画改编动画',
     weekDayCN: 7
@@ -80,7 +80,7 @@ export const ONAIR_2026S2 = {
   474371: {
     origin: 'MAPPA',
     tag: '战斗/血腥/猎奇',
-    timeCN: '',
+    timeCN: '2200',
     title: '异兽魔都 第2期',
     type: '漫画改编动画',
     weekDayCN: 3
@@ -89,7 +89,7 @@ export const ONAIR_2026S2 = {
   515856: {
     origin: 'BN Pictures/Actas',
     tag: '奇幻/魔法学园',
-    timeCN: '',
+    timeCN: '1530',
     title: '杖与剑的魔剑谭 第2期',
     type: '漫画改编动画',
     weekDayCN: 7
@@ -98,8 +98,8 @@ export const ONAIR_2026S2 = {
   565304: {
     origin: 'studio A-CAT',
     tag: '转生/奇幻/冒险',
-    timeCN: '',
-    title: '565304',
+    timeCN: '2345',
+    title: '三岁开始做王者 第2期',
     type: '漫画改编动画',
     weekDayCN: 3
   },
@@ -107,7 +107,7 @@ export const ONAIR_2026S2 = {
   517027: {
     origin: 'SILVER LINK.',
     tag: '间谍/异能/动作/恋爱喜剧/家庭',
-    timeCN: '',
+    timeCN: '1600',
     title: '夜樱家的大作战 第2期',
     type: '漫画改编动画',
     weekDayCN: 6
@@ -116,7 +116,7 @@ export const ONAIR_2026S2 = {
   533027: {
     origin: 'TMS娱乐',
     tag: '恋爱喜剧/拉扯',
-    timeCN: '',
+    timeCN: '2100',
     title: '租借女友 第5期',
     type: '漫画改编动画',
     weekDayCN: 3
@@ -125,7 +125,7 @@ export const ONAIR_2026S2 = {
   607043: {
     origin: 'DLE',
     tag: '奇妙日常/治愈',
-    timeCN: '',
+    timeCN: '0600',
     title: '小学3年级阿贝 QQ小芝麻',
     type: '漫画改编动画',
     weekDayCN: 7
@@ -134,7 +134,7 @@ export const ONAIR_2026S2 = {
   616378: {
     origin: 'ENGI',
     tag: '奇妙日常/治愈',
-    timeCN: '',
+    timeCN: '0600',
     title: '小鲨鱼去郊游 第2期',
     type: '漫画改编动画',
     weekDayCN: 7
@@ -143,16 +143,16 @@ export const ONAIR_2026S2 = {
   454438: {
     origin: 'TMS娱乐',
     tag: '重制/血腥/复仇',
-    timeCN: '',
+    timeCN: '0000',
     title: '新 北斗神拳',
     type: '漫画改编动画',
-    weekDayCN: 5
+    weekDayCN: 6
   },
 
   570448: {
     origin: 'SUNRISE',
     tag: '时穿/妖怪/恋爱',
-    timeCN: '',
+    timeCN: '2245',
     title: '摩绪',
     type: '漫画改编动画',
     weekDayCN: 6
@@ -161,7 +161,7 @@ export const ONAIR_2026S2 = {
   568572: {
     origin: 'BONES FILM',
     tag: '战斗/异能/妖怪',
-    timeCN: '',
+    timeCN: '2230',
     title: '黄泉的使者',
     type: '漫画改编动画',
     weekDayCN: 6
@@ -170,7 +170,7 @@ export const ONAIR_2026S2 = {
   377130: {
     origin: 'BUG FILMS',
     tag: '奇幻/魔法/师徒',
-    timeCN: '',
+    timeCN: '2200',
     title: '尖帽子的魔法工坊',
     type: '漫画改编动画',
     weekDayCN: 1
@@ -179,7 +179,7 @@ export const ONAIR_2026S2 = {
   576351: {
     origin: 'LIDENFILMS',
     tag: '魔法/师徒/搞笑',
-    timeCN: '',
+    timeCN: '2230',
     title: '黑猫和魔女的课堂',
     type: '漫画改编动画',
     weekDayCN: 7
@@ -188,7 +188,7 @@ export const ONAIR_2026S2 = {
   516543: {
     origin: '寿门堂',
     tag: '奇幻/婚姻/反差',
-    timeCN: '',
+    timeCN: '2030',
     title: '女骑士成为蛮族的新娘',
     type: '漫画改编动画',
     weekDayCN: 4
@@ -197,16 +197,16 @@ export const ONAIR_2026S2 = {
   552589: {
     origin: 'StudioKAI',
     tag: '神魔/恋爱喜剧',
-    timeCN: '',
+    timeCN: '0000',
     title: '迦楠大人的白给是恶魔级',
     type: '漫画改编动画',
-    weekDayCN: 6
+    weekDayCN: 7
   },
 
   505258: {
     origin: 'SANZIGEN',
     tag: '战斗/热血/抽象',
-    timeCN: '',
+    timeCN: '2300',
     title: '公鸡斗士',
     type: '漫画改编动画',
     weekDayCN: 7
@@ -215,7 +215,7 @@ export const ONAIR_2026S2 = {
   571555: {
     origin: 'StudioKAI',
     tag: '科幻/机战/怪兽',
-    timeCN: '',
+    timeCN: '2230',
     title: '冻结地球',
     type: '漫画改编动画',
     weekDayCN: 5
@@ -224,7 +224,7 @@ export const ONAIR_2026S2 = {
   623851: {
     origin: 'Studio Kafka',
     tag: '虚构/分裂/战争',
-    timeCN: '',
+    timeCN: '2000',
     title: '日本三国',
     type: '漫画改编动画',
     weekDayCN: 7
@@ -233,7 +233,7 @@ export const ONAIR_2026S2 = {
   506149: {
     origin: 'BENTEN Film',
     tag: '异能/战斗/转世',
-    timeCN: '',
+    timeCN: '2330',
     title: '轮回的花瓣',
     type: '漫画改编动画',
     weekDayCN: 4
@@ -242,7 +242,7 @@ export const ONAIR_2026S2 = {
   493804: {
     origin: 'Studio云雀',
     tag: '奇妙生物/喜剧',
-    timeCN: '',
+    timeCN: '2230',
     title: '库兹马在家唱歌哆啰啰',
     type: '漫画改编动画',
     weekDayCN: 4
@@ -251,7 +251,7 @@ export const ONAIR_2026S2 = {
   580133: {
     origin: 'Madhouse',
     tag: '智斗/心理/推理',
-    timeCN: '',
+    timeCN: '2300',
     title: '欺诈游戏',
     type: '漫画改编动画',
     weekDayCN: 1
@@ -260,7 +260,7 @@ export const ONAIR_2026S2 = {
   609175: {
     origin: 'Satelight',
     tag: '葡萄酒/遗产',
-    timeCN: '',
+    timeCN: '2230',
     title: '神之水滴',
     type: '漫画改编动画',
     weekDayCN: 5
@@ -269,7 +269,7 @@ export const ONAIR_2026S2 = {
   590353: {
     origin: 'BONES FILM',
     tag: '杀手/异能/婚姻',
-    timeCN: '',
+    timeCN: '2200',
     title: '婚姻剧毒',
     type: '漫画改编动画',
     weekDayCN: 2
@@ -278,7 +278,7 @@ export const ONAIR_2026S2 = {
   581284: {
     origin: 'CUE',
     tag: '杀手/校园/喜剧',
-    timeCN: '',
+    timeCN: '2200',
     title: '杀手青春',
     type: '漫画改编动画',
     weekDayCN: 6
@@ -287,7 +287,7 @@ export const ONAIR_2026S2 = {
   409468: {
     origin: 'SIGNAL.MD/Production I.G',
     tag: '群像/职场/美术',
-    timeCN: '',
+    timeCN: '2300',
     title: '左撇子艾伦',
     type: '漫画改编动画',
     weekDayCN: 2
@@ -296,7 +296,7 @@ export const ONAIR_2026S2 = {
   576121: {
     origin: 'ZEXCS',
     tag: '落语/师徒/成长',
-    timeCN: '',
+    timeCN: '2230',
     title: '朱音落语',
     type: '漫画改编动画',
     weekDayCN: 6
@@ -305,16 +305,16 @@ export const ONAIR_2026S2 = {
   493724: {
     origin: 'Madhouse',
     tag: '歌剧/青春/群像',
-    timeCN: '',
+    timeCN: '0045',
     title: '淡岛百景',
     type: '漫画改编动画',
-    weekDayCN: 4
+    weekDayCN: 5
   },
 
   580473: {
     origin: 'EMT Squared/MagicBUS',
     tag: '日常/美食/治愈',
-    timeCN: '',
+    timeCN: '2300',
     title: '女仆小姐的贪吃日常',
     type: '漫画改编动画',
     weekDayCN: 7
@@ -323,7 +323,7 @@ export const ONAIR_2026S2 = {
   535669: {
     origin: 'StudioKAI',
     tag: '校园/恋爱/反差',
-    timeCN: '',
+    timeCN: '2256',
     title: '冰之城墙',
     type: '漫画改编动画',
     weekDayCN: 4
@@ -332,7 +332,7 @@ export const ONAIR_2026S2 = {
   513208: {
     origin: 'Satelight',
     tag: '校园/恋爱喜剧',
-    timeCN: '',
+    timeCN: '2326',
     title: '雾尾粉丝后援会',
     type: '漫画改编动画',
     weekDayCN: 4
@@ -341,7 +341,7 @@ export const ONAIR_2026S2 = {
   564289: {
     origin: 'Felix Film',
     tag: '校园/恋爱喜剧',
-    timeCN: '',
+    timeCN: '2200',
     title: '想结束这场“我爱你”的游戏',
     type: '漫画改编动画',
     weekDayCN: 2
@@ -350,7 +350,7 @@ export const ONAIR_2026S2 = {
   551123: {
     origin: 'ZERO-G',
     tag: '校园/恋爱喜剧',
-    timeCN: '',
+    timeCN: '2230',
     title: '木头风纪委员和迷你裙JK的故事',
     type: '漫画改编动画',
     weekDayCN: 1
@@ -359,7 +359,7 @@ export const ONAIR_2026S2 = {
   590393: {
     origin: 'TMS娱乐',
     tag: '校园/恋爱喜剧',
-    timeCN: '',
+    timeCN: '2245',
     title: '哪里有温柔对待阿宅的辣妹',
     type: '漫画改编动画',
     weekDayCN: 3
@@ -368,16 +368,16 @@ export const ONAIR_2026S2 = {
   610703: {
     origin: '龙之子工作室',
     tag: '邻里/恋爱喜剧',
-    timeCN: '',
+    timeCN: '0029',
     title: '当前 正被打扰中',
     type: '漫画改编动画',
-    weekDayCN: 2
+    weekDayCN: 3
   },
 
   526789: {
     origin: "Brain's Base",
     tag: '恋爱喜剧/福利',
-    timeCN: '',
+    timeCN: '2030',
     title: '弱弱老师',
     type: '漫画改编动画',
     weekDayCN: 6
@@ -386,16 +386,16 @@ export const ONAIR_2026S2 = {
   569161: {
     origin: 'PRA',
     tag: '萌系/日常/福利',
-    timeCN: '',
+    timeCN: '0100',
     title: '一叠间漫画咖啡屋生活',
     type: '漫画改编动画',
-    weekDayCN: 6
+    weekDayCN: 7
   },
 
   543360: {
     origin: 'Soigne',
     tag: '酒/日常/百合',
-    timeCN: '',
+    timeCN: '2300',
     title: '上伊那牡丹 酒醉身姿似百合',
     type: '漫画改编动画',
     weekDayCN: 5
@@ -404,7 +404,7 @@ export const ONAIR_2026S2 = {
   508455: {
     origin: 'Drive',
     tag: '校园/恋爱/BL',
-    timeCN: '',
+    timeCN: '2330',
     title: '加油吧 中村君',
     type: '漫画改编动画',
     weekDayCN: 3
@@ -413,25 +413,25 @@ export const ONAIR_2026S2 = {
   631963: {
     origin: 'NAZ',
     tag: '神魔/灵魂/喜剧',
-    timeCN: '',
+    timeCN: '2355',
     title: '蒲公英',
     type: '漫画改编动画',
-    weekDayCN: 0
+    weekDayCN: 4
   },
 
   621602: {
     origin: 'Elias',
     tag: '搞笑/抽象/涩涩',
-    timeCN: '',
+    timeCN: '0010',
     title: '淫狱团地',
     type: '漫画改编动画',
-    weekDayCN: 7
+    weekDayCN: 1
   },
 
   609183: {
     origin: 'studio LEO',
     tag: '恋爱喜剧/抽象',
-    timeCN: '',
+    timeCN: '2355',
     title: '茉莉花酱的好感度正在崩坏',
     type: '漫画改编动画',
     weekDayCN: 7
@@ -440,7 +440,7 @@ export const ONAIR_2026S2 = {
   547888: {
     origin: 'WHITE FOX',
     tag: '穿越/冒险/轮回',
-    timeCN: '',
+    timeCN: '2100',
     title: 'Re:从零开始的异世界生活 第4期',
     type: '小说改编动画',
     weekDayCN: 3
@@ -449,7 +449,7 @@ export const ONAIR_2026S2 = {
   469104: {
     origin: 'WIT Studio',
     tag: '奇幻/情感/成长',
-    timeCN: '',
+    timeCN: '1630',
     title: '小书痴的下克上 第4期 第三部 领主的养女',
     type: '小说改编动画',
     weekDayCN: 6
@@ -458,7 +458,7 @@ export const ONAIR_2026S2 = {
   515594: {
     origin: '8bit',
     tag: '奇幻/群像/傲天',
-    timeCN: '',
+    timeCN: '2200',
     title: '关于我转生变成史莱姆这档事第4期 Part.1',
     type: '小说改编动画',
     weekDayCN: 5
@@ -467,7 +467,7 @@ export const ONAIR_2026S2 = {
   585790: {
     origin: 'Studio五组/AXsiZ',
     tag: '转生/奇幻/逆天',
-    timeCN: '',
+    timeCN: '2100',
     title: '转生成自动贩卖机的我今天也在迷宫徘徊 第3期',
     type: '小说改编动画',
     weekDayCN: 3
@@ -476,16 +476,16 @@ export const ONAIR_2026S2 = {
   575057: {
     origin: 'ZERO-G',
     tag: '穿越/种田/后宫',
-    timeCN: '',
+    timeCN: '2200',
     title: '异世界悠闲农家 第2期',
     type: '小说改编动画',
     weekDayCN: 1
   },
 
   567234: {
-    origin: '',
+    origin: 'OLM Team Yoshioka',
     tag: '转生/乙游/奇幻',
-    timeCN: '',
+    timeCN: '2100',
     title: '身为悲剧始作俑者的最强邪恶BOSS女王为民竭心尽力 第2期',
     type: '小说改编动画',
     weekDayCN: 2
@@ -494,7 +494,7 @@ export const ONAIR_2026S2 = {
   510710: {
     origin: 'Lerche',
     tag: '校园/群像/情感',
-    timeCN: '',
+    timeCN: '2130',
     title: '欢迎来到实力至上主义教室 第4期',
     type: '小说改编动画',
     weekDayCN: 3
@@ -503,7 +503,7 @@ export const ONAIR_2026S2 = {
   458684: {
     origin: 'project No.9',
     tag: '校园/恋爱/日常',
-    timeCN: '',
+    timeCN: '2130',
     title: '关于邻家的天使大人不知不觉把我惯成了废人这件事 第2期',
     type: '小说改编动画',
     weekDayCN: 5
@@ -512,7 +512,7 @@ export const ONAIR_2026S2 = {
   551455: {
     origin: 'WIT Studio',
     tag: '季节/情感/战斗',
-    timeCN: '',
+    timeCN: '2300',
     title: '春夏秋冬代行者 春之舞',
     type: '小说改编动画',
     weekDayCN: 6
@@ -521,7 +521,7 @@ export const ONAIR_2026S2 = {
   456079: {
     origin: 'CONNECT',
     tag: '校园/恋爱喜剧',
-    timeCN: '',
+    timeCN: '2230',
     title: '和班上第二可爱的女孩子成为朋友',
     type: '小说改编动画',
     weekDayCN: 2
@@ -530,7 +530,7 @@ export const ONAIR_2026S2 = {
   537722: {
     origin: 'Voil',
     tag: '恋爱/青春伤痛',
-    timeCN: '',
+    timeCN: '2330',
     title: '复制品也想谈恋爱',
     type: '小说改编动画',
     weekDayCN: 2
@@ -539,43 +539,43 @@ export const ONAIR_2026S2 = {
   567418: {
     origin: 'Studio COMET',
     tag: '校园/恋爱/党争',
-    timeCN: '',
+    timeCN: '0028',
     title: '灰原君的青春二周目',
     type: '小说改编动画',
-    weekDayCN: 4
+    weekDayCN: 5
   },
 
   608227: {
     origin: 'LIDENFILMS',
     tag: '侦探/推理/异能',
-    timeCN: '',
+    timeCN: '0058',
     title: '又被杀掉了呢 侦探大人',
     type: '小说改编动画',
-    weekDayCN: 4
+    weekDayCN: 5
   },
 
   570584: {
     origin: 'JUVENAGE',
     tag: '神魔/除灵/日常',
-    timeCN: '',
+    timeCN: '0030',
     title: '楠木邸的神明庭院',
     type: '小说改编动画',
-    weekDayCN: 6
+    weekDayCN: 7
   },
 
   623854: {
     origin: 'Qzil.la/S.o.K',
     tag: '转生/搞笑/抽象',
-    timeCN: '',
+    timeCN: '0059',
     title: '请求女神让我转生成勇者的肋骨',
     type: '小说改编动画',
-    weekDayCN: 2
+    weekDayCN: 3
   },
 
   599171: {
     origin: 'Studio Flad',
     tag: '穿越/奇幻/冒险',
-    timeCN: '',
+    timeCN: '2100',
     title: '最强职业不是勇者也不是贤者好像是鉴定士(伪)的样子',
     type: '小说改编动画',
     weekDayCN: 6
@@ -584,7 +584,7 @@ export const ONAIR_2026S2 = {
   604307: {
     origin: 'TROYCA',
     tag: '恋爱/喜剧/退婚',
-    timeCN: '',
+    timeCN: '2300',
     title: '关于逃走的鱼很大但钓上来的鱼更大这件事',
     type: '小说改编动画',
     weekDayCN: 3
@@ -593,7 +593,7 @@ export const ONAIR_2026S2 = {
   558088: {
     origin: '苇Production',
     tag: '穿越/恋爱/喜剧',
-    timeCN: '',
+    timeCN: '2100',
     title: '自称恶役千金的婚约者观察记录',
     type: '小说改编动画',
     weekDayCN: 1
@@ -602,7 +602,7 @@ export const ONAIR_2026S2 = {
   606263: {
     origin: 'Yostar Pictures',
     tag: '主播/电波/魔改',
-    timeCN: '',
+    timeCN: '2330',
     title: '主播女孩重度依赖',
     type: '游戏改编动画',
     weekDayCN: 6
@@ -611,7 +611,7 @@ export const ONAIR_2026S2 = {
   520633: {
     origin: 'Production I.G',
     tag: '奇妙日常/治愈',
-    timeCN: '',
+    timeCN: '0825',
     title: '轻松熊',
     type: '其他题材改编动画',
     weekDayCN: 6
@@ -620,7 +620,7 @@ export const ONAIR_2026S2 = {
   575559: {
     origin: 'Uwan Pictures',
     tag: '反差/福利/涩涩',
-    timeCN: '',
+    timeCN: '2355',
     title: '一脸嫌弃表情的妹子给你看胖次R',
     type: '其他题材改编动画',
     weekDayCN: 0
