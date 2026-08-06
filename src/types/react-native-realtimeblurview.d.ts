23351cba2553d6e8529a09fcd7c2f526b9470017
czy0729 <402731062@qq.com>
2026-08-07 00:06:42 +0800

    - [优化] 移除 Babel 别名插件改用 Metro 原生 tsconfig 路径解析，整理类型声明与忽略规则

--- src/types/react-native-realtimeblurview.d.ts
+++ src/types/react-native-realtimeblurview.d.ts
@@ -4,18 +4,20 @@
  * @Last Modified by: czy0729
  * @Last Modified time: 2026-06-22 19:43:53
  */
-import React from 'react'
+declare module 'react-native-realtimeblurview' {
+  import React from 'react'
 
-import type { ViewProps } from 'react-native'
+  import type { ViewProps } from 'react-native'
 
-interface BlurRootProps extends ViewProps {
-  name?: string
+  interface BlurRootProps extends ViewProps {
+    name?: string
+  }
+
+  interface BlurProps extends ViewProps {
+    blurNode?: string
+    radius?: number
+  }
+
+  export class BlurRootView extends React.Component<BlurRootProps> {}
+  export class BlurView extends React.Component<BlurProps> {}
 }
-
-interface BlurProps extends ViewProps {
-  blurNode?: string
-  radius?: number
-}
-
-export class BlurRootView extends React.Component<BlurRootProps> {}
-export class BlurView extends React.Component<BlurProps> {}
