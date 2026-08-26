ea05b86cf9df06e8468d425465b35bb8b51ffccf
czy0729 <402731062@qq.com>
2026-08-26 08:05:28 +0800

    - [修复] worklets: IPA (非 expo iOS) 降级复用 reanimated 的 runOnJS/runOnUI, 抽离安卓实现至 reanimated.ts 与 index.android.ts 共用, 规避 IPA 缺失 react-native-worklets 原生模块

--- src/utils/worklets/index.android.ts
+++ src/utils/worklets/index.android.ts
@@ -2,23 +2,6 @@
  * @Author: czy0729
  * @Date: 2026-08-17 10:00:00
  * @Last Modified by: czy0729
- * @Last Modified time: 2026-08-20 04:08:51
+ * @Last Modified time: 2026-08-26 06:42:23
  */
-import { runOnJS, runOnUI } from 'react-native-reanimated'
-
-/** 在 UI worklet 中调度函数到 JS 线程执行 (Android 降级环境无 worklets, 用 reanimated 的 runOnJS) */
-export function scheduleOnRN<Args extends unknown[]>(
-  fun: (...args: Args) => void,
-  ...args: Args
-): void {
-  'worklet'
-  runOnJS(fun)(...args)
-}
-
-/** 在 JS 线程调度 worklet 到 UI 线程执行 (Android 降级环境无 worklets, 用 reanimated 的 runOnUI) */
-export function scheduleOnUI<Args extends unknown[], ReturnValue>(
-  worklet: (...args: Args) => ReturnValue,
-  ...args: Args
-): void {
-  runOnUI(worklet)(...args)
-}
+export * from './reanimated'
--- src/utils/worklets/index.ts
+++ src/utils/worklets/index.ts
@@ -2,7 +2,23 @@
  * @Author: czy0729
  * @Date: 2026-08-17 10:00:00
  * @Last Modified by: czy0729
- * @Last Modified time: 2026-08-20 04:08:53
+ * @Last Modified time: 2026-08-26 08:03:33
  */
-/** iOS 使用 react-native-worklets 原生的 scheduleOnRN/scheduleOnUI 调度实现 */
-export { scheduleOnRN, scheduleOnUI } from 'react-native-worklets'
+import { IOS_IPA } from '@src/config'
+import {
+  scheduleOnRN as reanimatedScheduleOnRN,
+  scheduleOnUI as reanimatedScheduleOnUI
+} from './reanimated'
+
+/** IPA (非 expo 的 iOS) 未安装 react-native-worklets 原生模块, 复用 reanimated 实现, 与 android 一致 */
+export const scheduleOnRN = (
+  IOS_IPA
+    ? reanimatedScheduleOnRN
+    : (require('react-native-worklets') as typeof import('react-native-worklets')).scheduleOnRN
+) as typeof reanimatedScheduleOnRN
+
+export const scheduleOnUI = (
+  IOS_IPA
+    ? reanimatedScheduleOnUI
+    : (require('react-native-worklets') as typeof import('react-native-worklets')).scheduleOnUI
+) as typeof reanimatedScheduleOnUI
