1d7838becc5dea747dc7c353c5783c62fcd82efa
czy0729 <402731062@qq.com>
2026-08-28 22:05:33 +0800

    - [优化] Metro 构建性能优化: 开启 transform 缓存持久化与 inlineRequires, watchman 忽略产物目录, dev 脚本限制 max-workers

--- metro.config.js
+++ metro.config.js
@@ -2,25 +2,36 @@
  * @Author: czy0729
  * @Date: 2023-04-15 04:37:50
  * @Last Modified by: czy0729
- * @Last Modified time: 2026-04-21 13:05:10
+ * @Last Modified time: 2026-08-28 21:23:41
  */
 /** Learn more https://docs.expo.io/guides/customizing-metro */
 const fs = require('fs')
 const path = require('path')
 const { getDefaultConfig } = require('expo/metro-config')
+const { FileStore } = require('metro-cache')
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname)
 
-// const FileStore = require('metro-cache').FileStore
-// config.cacheStores = [new FileStore({ root: path.join(__dirname, 'metro-cache') })]
+// transform 缓存持久化到项目目录 (默认在 /tmp, 重启即丢), 二次启动 Metro 秒命中
+config.cacheStores = [new FileStore({ root: path.join(__dirname, 'metro-cache') })]
 
 const monorepoPackages = {
   stream: require.resolve('stream-browserify')
 }
 
 config.resolver.extraNodeModules = monorepoPackages
-config.resolver.blacklistRE = [/packages\/.*/]
+// 构建产物与原生工程目录不参与解析与监听; 需锚定项目根, 否则会误伤 node_modules 内的同名目录 (如各包的 dist/)
+const projectBlockList = ['dist', 'web', 'ios', 'android'].map(
+  dir =>
+    new RegExp(
+      `${path
+        .join(__dirname, dir)
+        .replace(/[/\\]/g, '/')
+        .replace(/[.*+?^${}()|[\]\\]/g, '\\$&')}/.*`
+    )
+)
+config.resolver.blacklistRE = [/packages\/.*/, ...projectBlockList]
 config.resolver.assetExts.push('proto', 'bin')
 
 // Metro 0.76 不支持 tsconfig paths，从 tsconfig.json 动态读取别名映射
@@ -56,5 +67,13 @@ config.resolver.resolveRequest = (context, moduleName, platform) => {
 
   return context.resolveRequest(context, moduleName, platform)
 }
+
+// Expo 默认 inlineRequires: false (全量模块同步 eval), 开启后模块首次使用时才执行, 首帧 eval 量大幅下降
+config.transformer.getTransformOptions = async () => ({
+  transform: {
+    experimentalImportSupport: true,
+    inlineRequires: true
+  }
+})
 
 module.exports = config
